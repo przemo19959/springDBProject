f3e9473766bc312e83190d72ac37354db614dd22
przemo19959 <przemo1892@gmail.com>
2019-12-15 18:17:03 +0100

    Dodano obsługę zmiany stylu pola id. Dodano obsługę komunikatów jeśli wprowadzono błędne dane do Cbox lub pola numerycznego (naciśnięcie przycisku findById). Funkcja findById działa poprawnie.
    Pole input przyjmuje jedynie wartości numeryczne.

--- WebContent/js/hello.js
+++ WebContent/js/hello.js
@@ -2,7 +2,6 @@ var myApp = angular.module('demo', []);
 var mainURL = "http://localhost:8080/springDBProject/mainPage/";
 
 myApp.controller('Hello', ['$scope', '$http', function ($scope, $http) {
-	$scope.id = 0;
 	$scope.name = "nothing";
 
 	$scope.records = [];
@@ -22,11 +21,13 @@ myApp.controller('Hello', ['$scope', '$http', function ($scope, $http) {
 
 	$scope.tableCBox = $scope.tables[0];
 
+	$scope.idInputStyle = "error";
+
 	$scope.toString = function (obj) {
-		var keys=Object.keys(obj);
+		var keys = Object.keys(obj);
 		if (keys.length > 0 && typeof obj != 'string') {
 			var result = [], i;
-			for (i=1;i<keys.length;i++) {
+			for (i = 1; i < keys.length; i++) {
 				result.push(obj[keys[i]]);
 			}
 			return result.join(", ");
@@ -39,22 +40,36 @@ myApp.controller('Hello', ['$scope', '$http', function ($scope, $http) {
 			$http.get(mainURL + $scope.tableCBox.value)
 				.then(function (response) {
 					console.log(response.data);
-					$scope.records = response.data;
 					if (response.data.length > 0) {
+						$scope.records = response.data;
 						$scope.columns = Object.keys($scope.records[0]);
+					} else {
+						alert("Table " + $scope.tableCBox.name + " is empty!");
 					}
 				});
 		}
 	}
 
 	$scope.findById = function () {
-		console.log($scope.id);
-		// if($scope.tableCBox!=$scope.tables[0]){
-		// 	$http.get("http://localhost:8080/springDBProject/mainPage/"+$scope.tableCBox.value)
-		// 	.then(function(response){
-		// 		console.log(response);
-		// 		$scope.records=response.data;
-		// 	});
-		// }
+		if ($scope.tableCBox != $scope.tables[0] && $scope.id > 0) {
+			$http.get(mainURL + $scope.tableCBox.value + "/" + $scope.id)
+				.then(function (response) {
+					if (response.status!=404) {
+						$scope.records=[];
+						$scope.records[0] = response.data;
+						$scope.columns = Object.keys($scope.records[0]);
+					} else {
+						alert("Table " + $scope.tableCBox.name + " doesn't contain record with id=" + $scope.id + "!");
+					}
+				});
+		} else if ($scope.tableCBox == $scope.tables[0]) {
+			alert("Table was't chosen!");
+		} else if ($scope.id < 1 || $scope.id == undefined) {
+			alert("Id value must be greater than 0, but is " + $scope.id + "!");
+		}
+	}
+
+	$scope.idFieldChanged = function () {
+		$scope.idInputStyle = ($scope.id > 0) ? "correct" : "error";
 	}
 }]);
