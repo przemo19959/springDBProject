b9e32735018449d22d2e9b2f24b654bbd9ab31b6
cp24 <werewolf3@op.pl>
2020-08-06 15:02:57 +0200

    Small fix in database dao test

--- src/test/java/application/dao/DatabaseDaoTest.java
+++ src/test/java/application/dao/DatabaseDaoTest.java
@@ -1,13 +1,10 @@
 package application.dao;
 
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
